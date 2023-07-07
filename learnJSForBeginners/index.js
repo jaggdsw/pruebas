ea587dbec61d8056172c5f0e34e74427cdb4cc87
pepito <pepito@grillo.com>
2023-07-07 19:33:47 +0200

    second commit

---
+++ learnJSForBeginners/index.js
@@ -0,0 +1,17 @@
+message = "Hello World!"
+console.log(message)
+message = "Nice weather!"
+console.log(message)
+if(true) {
+    let names = "Nathan";
+}
+console.log(names)
+if(true) {
+    var names = "Nathan";
+}
+console.log(names);
+
+let price = "50";
+let tax = 5;
+let totalPrice = Number(price) + Number(tax);
+console.log(totalPrice);
